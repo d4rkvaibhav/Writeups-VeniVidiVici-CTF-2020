dc7322ed7ce0bddcd131854cfc77a98bd7d56b66
Vaibhav Jayant <43247883+d4rkvaibhav@users.noreply.github.com>
2020-02-18 13:57:31 +0530

    Update conversion.py

--- misc/RunitifYoucan/conversion.py
+++ misc/RunitifYoucan/conversion.py
@@ -1,15 +1,11 @@
 f=open("challenge.exe").read()
-# q1=open("data.txt","w+")
 s=""
 for i in range(len(f)):
-	q=ord(f[i])^30
-	if(q==106):
-		q=0
-	p=hex(q)[2:]
-	if(len(p)<2):
-		p='0'+p
-	# q1.write(p+" ")
-	s+=p
-# print
-# open("robot.png","wb").write(open("new").read()[::-1].decode('hex'))
+ q=ord(f[i])^30
+ if(q==106):
+  q=0
+ p=hex(q)[2:]
+ if(len(p)<2):
+  p='0'+p
+ s+=p
 open("ans.exe","wb").write(s.decode('hex'))
